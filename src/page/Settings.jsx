2f3270a7d1b3c39f2d13899d26ac338a4385069c
yjuwaheer <yjuwaheer@mun.ca>
2022-06-24 17:11:28 -0230

    Added settings options

--- src/page/Settings.jsx
+++ src/page/Settings.jsx
@@ -1,7 +1,148 @@
 import React from "react";
+// Chakra UI
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Divider,
+  Text,
+  Button,
+  Input,
+  Select,
+} from "@chakra-ui/react";
+// Country list
+import countryList from "react-select-country-list";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <div className="flex justify-center mt-10">
+      <Tabs variant="soft-rounded" colorScheme="orange" className="w-1/2">
+        <TabList>
+          <Tab>Account</Tab>
+          <Tab>Appearance</Tab>
+        </TabList>
+        <Divider className="mt-4" />
+        <TabPanels>
+          <TabPanel className="w-full flex flex-col items-start">
+            <Text
+              fontSize="lg"
+              className="font-medium my-3 underline underline-offset-1"
+            >
+              Email
+            </Text>
+            <Input
+              variant="filled"
+              placeholder="Email"
+              isDisabled={true}
+              value="example@gmail.com"
+              onChange={() => {}}
+              className="mb-3"
+            />
+
+            <div className="flex w-full mb-3">
+              <div className="flex flex-col items-start w-1/2 mr-4">
+                <Text
+                  fontSize="lg"
+                  className="font-medium my-3 underline underline-offset-1"
+                >
+                  First Name
+                </Text>
+                <Input
+                  variant="filled"
+                  placeholder="First Name"
+                  value=""
+                  onChange={() => {}}
+                />
+              </div>
+
+              <div className="flex flex-col items-start w-1/2">
+                <Text
+                  fontSize="lg"
+                  className="font-medium my-3 underline underline-offset-1"
+                >
+                  Last Name
+                </Text>
+                <Input
+                  variant="filled"
+                  placeholder="Last Name"
+                  value=""
+                  onChange={() => {}}
+                />
+              </div>
+            </div>
+
+            <Text
+              fontSize="lg"
+              className="font-medium my-3 underline underline-offset-1"
+            >
+              Username
+            </Text>
+            <Input
+              variant="filled"
+              placeholder="Username"
+              value=""
+              className="mb-3"
+              onChange={() => {}}
+            />
+
+            <Text
+              fontSize="lg"
+              className="font-medium my-3 underline underline-offset-1"
+            >
+              Phone Number
+            </Text>
+            <Input
+              variant="filled"
+              placeholder="Phone Number"
+              value=""
+              className="mb-3"
+              onChange={() => {}}
+            />
+
+            <Text
+              fontSize="lg"
+              className="font-medium my-3 underline underline-offset-1"
+            >
+              Country
+            </Text>
+            <Select
+              variant="filled"
+              placeholder="Select Country"
+              value=""
+              className="mb-3"
+              onChange={() => {}}
+            >
+              {countryList()
+                .getData()
+                .map((country) => (
+                  <option value={country.value} key={country.value}>
+                    {country.label} - {country.value}
+                  </option>
+                ))}
+            </Select>
+          </TabPanel>
+
+          <TabPanel className="w-full flex flex-col items-start">
+            <Text
+              fontSize="lg"
+              className="font-medium mb-3 underline underline-offset-1"
+            >
+              Choose your accent color
+            </Text>
+            <div>
+              <Button colorScheme="orange" className="mr-3"></Button>
+              <Button colorScheme="red" className="mr-3"></Button>
+              <Button colorScheme="yellow" className="mr-3"></Button>
+              <Button colorScheme="green" className="mr-3"></Button>
+              <Button colorScheme="blue" className="mr-3"></Button>
+            </div>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Settings;
